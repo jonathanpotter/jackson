bc8491dedf46e5a8e3e0f2ffb61c77c04d57d35a
Jonathan Potter <jonpot@umich.edu>
2022-12-13 08:11:26 -0500

    Add pick family game.

--- README.md
+++ README.md
@@ -7,4 +7,5 @@ Some short programs that Jackson and Jonathan wrote while learning Python.
 ```
 python3 ./cardGame.py
 python3 ./guessingGame.py
+python3 ./pickFamilyGame.py
 ```
---
+++ pickFamilyGame.py
@@ -0,0 +1,42 @@
+import random
+
+
+games = [
+    'Tenzi',
+    'Card Games',
+    'Pokemon',
+    'Uno',
+    'Kids Against Maturity',
+    'Do You Really Know Your Family',
+    'Catan',
+    'Suspend',
+    'Connect Four',
+    'Battleship',
+    'Blokus',
+    'Chess',
+    'Checker',
+    'Dominoes',
+    'Monopoly',
+    'Game of Life',
+    'Clue',
+    'Risk',
+    'Sorry',
+    'Titanic',
+    'Scrabble',
+    'Rush hour',
+    'Rummikub',
+    'Mancala',
+    'Mastermind',
+    'Trivial Pursuit Harry Potter',
+    'Rat a tat cat',
+    'Sleeping Queens',
+    'Speak Out',
+    'Rubik’s Race',
+    'Guesstures',
+    'Rock Em Sock Em',
+    'Snap Circuits',
+    'Mouse Trap',
+    'Backgammon'
+]
+
+print("Tonight, we are going to play: " + random.choice(games))
